af1e0588f6d8f21d5eb107cb16fd52f828d5b213
Sven Schultschik <sven@schultschik.de>
2019-08-04 23:32:52 +0200

    #38 start with the installer script
    #38 working version of install script

--- administrator/components/com_schuweb_sitemap/helpers/schuweb_sitemap.php
+++ administrator/components/com_schuweb_sitemap/helpers/schuweb_sitemap.php
@@ -36,4 +36,27 @@ class SchuWeb_SitemapHelper
             'index.php?option=com_plugins&view=plugins&filter_folder=schuweb_sitemap',
             $vName == 'extensions');
     }
+	/**
+	 * Returns a list of installed extension, where SchuWeb sitemap has the fitting plugin installed
+	 *
+	 * @return mixed
+	 *
+	 * @since __DEPLOY_VERSION__
+	 */
+	public static function getExtensionsList()
+	{
+		$db    = JFactory::getDbo();
+		$query = $db->getQuery(true);
+
+		$query->select('e.*')
+			->from($db->quoteName('#__extensions') . 'AS e')
+			->join('INNER', '#__extensions AS p ON e.element=p.element and p.enabled=0 and p.type=\'plugin\' and p.folder=\'schuweb_sitemap\'')
+			->where('e.type=\'component\' and e.enabled=1');
+
+		$db->setQuery($query);
+		$extensions = $db->loadObjectList();
+
+		return $extensions;
+	}
+
 }
