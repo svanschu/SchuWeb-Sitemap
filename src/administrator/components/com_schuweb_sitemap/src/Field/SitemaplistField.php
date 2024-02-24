1b138b2eba518d8be425778287b72fd1e3325211
Sven Schultschik <sven@schultschik.de>
2024-02-24 15:55:02 +0100

    add form field to create a select list of sitemaps
    Signed-off-by: Sven Schultschik <sven@schultschik.de>

---
+++ src/administrator/components/com_schuweb_sitemap/src/Field/SitemaplistField.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @version     sw.build.version
+ * @copyright   Copyright (C) 2024 Sven Schultschik. All rights reserved
+ * @license     GPL-3.0-or-later
+ * @author      Sven Schultschik (extensions@schultschik.de)
+ * @link        extensions.schultschik.de
+ */
+
+namespace SchuWeb\Component\Sitemap\Administrator\Field;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Form\Field\ListField;
+use Joomla\Database\DatabaseDriver;
+use Joomla\Database\DatabaseInterface;
+
+defined('_JEXEC') || die();
+
+/**
+ * Create a select list of all available sitemaps
+ * 
+ * @since __BUMP_VERSION__
+ */
+class SitemaplistField extends ListField
+{
+    /**
+     * Type of the field
+     * 
+     * @since __BUMP_VERSION__
+     */
+    protected $type = 'Sitemaplist';
+
+    /**
+     * Get the select list field information
+     * 
+     * @since __BUMP_VERSION__
+     */
+    protected function getInput()
+    {
+        // Add options for each and every sitemap
+        /** @var DatabaseDriver $db */
+        $db = method_exists($this, 'getDatabase')
+            ? $this->getDatabase()
+            : Factory::getContainer()->get(DatabaseInterface::class);
+
+        $query = $db->getQuery(true)
+            ->select([
+                $db->qn('id'),
+                $db->qn('title')
+            ])
+            ->from($db->qn('#__schuweb_sitemap'));
+        $db->setQuery($query);
+
+        $sitemaplist = $db->loadObjectList() ?? [];
+
+        foreach ($sitemaplist as $sitemap) {
+            $this->addOption($sitemap->title, ['value' => $sitemap->id]);
+        }
+
+        return parent::getInput();
+    }
+}
