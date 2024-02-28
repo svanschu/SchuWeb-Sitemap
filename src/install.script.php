e4e18f4fa7d47931bb4c6d95272d8d16d071658c
Sven Schultschik <sven@schultschik.de>
2024-02-28 22:10:16 +0100

    move sitemap generation on install to component script
    Signed-off-by: Sven Schultschik <sven@schultschik.de>

--- src/install.script.php
+++ src/install.script.php
@@ -99,64 +99,6 @@ class pkg_schuweb_sitemapInstallerScript extends InstallerScript
 				Factory::getApplication()->enqueueMessage(JText::_('SCHUWEB_SITEMAP_ACTIVATE_PLUGIN_FAILED'), 'error');
 			}
 		}
-
-		// Get all menus
-		$query = $db->getQuery(true);
-
-		$query->select($db->quoteName('menutype'))
-			->from($db->quoteName('#__menu_types'));
-
-		$db->setQuery($query);
-		$menus = $db->loadObjectList();
-
-		// Create a default sitemap with all needed components and all menus
-		$columns = array(
-			'title',
-			'alias',
-			'attribs',
-			'selections',
-			'is_default',
-			'state',
-			'access',
-		);
-
-		// {"mainmenu":{"enabled":"1","priority":"0.5","changefreq":"weekly"}}
-		$selections = array();
-
-		foreach ($menus as $menu)
-		{
-			$selections[$menu->menutype] = array(
-				"enabled"    => 1,
-				"priority"   => 1,
-				"changefreq" => "weekly",
-			);
-		}
-
-		$attribs = '{"showintro":"1","show_menutitle":"1","classname":"",'
-			. '"columns":"","compress_xml":"1",'
-			. '"include_link":"1","xmlLastMod":"1",'
-			. '"xmlInsertChangeFreq":"1","xmlInsertPriority":"1",'
-			. '"news_publication_name":""}';
-
-		$values = array(
-			$db->quote('Sitemap'),
-			$db->quote('sitemap'),
-			$db->quote($attribs),
-			$db->quote(json_encode($selections)),
-			1,
-			1,
-			1,
-		);
-
-		$query = $db->getQuery(true);
-
-		$query->insert($db->quoteName('#__schuweb_sitemap'))
-			->columns($db->quoteName($columns))
-			->values(implode(',', $values));
-
-		// Set the query using our newly populated query object and execute it.
-		$db->setQuery($query);
-		$db->execute();
 	}
 
 	/**
