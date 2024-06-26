1c1d0c16ad853660d66718524f28904cf49481b8
Sven Schultschik <sven@schultschik.de>
2024-06-27 01:05:09 +0200

    use namespace structure and new event based dispatcher for content plugin #132
    Signed-off-by: Sven Schultschik <sven@schultschik.de>

---
+++ src/plugins/schuweb_sitemap/content/services/provider.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @version     sw.build.version
+ * @copyright   Copyright (C) 2019 - 2024 Sven Schultschik. All rights reserved
+ * @license     GPL-3.0-or-later
+ * @author      Sven Schultschik (extensions@schultschik.de)
+ * @link        extensions.schultschik.de
+ */
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Extension\PluginInterface;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+use Joomla\Event\DispatcherInterface;
+use SchuWeb\Plugin\SchuWeb_Sitemap\Content\Extension\Content;
+
+    return new class() implements ServiceProviderInterface
+    {
+        public function register(Container $container)
+        {
+            $container->set(
+                PluginInterface::class,
+                function (Container $container) {
+    
+                    $config = (array) PluginHelper::getPlugin('schuweb_sitemap', 'content');
+                    $subject = $container->get(DispatcherInterface::class);
+                    $app = Factory::getApplication();
+                    
+                    $plugin = new Content($subject, $config);
+                    $plugin->setApplication($app);
+    
+                    return $plugin;
+                }
+            );
+        }
+    };
